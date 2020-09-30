21590ea6d2eab1a782a5cc57ff102f4bc63d1af1
Johannes Schindelin <johannes.schindelin@gmx.de>
2020-09-30 14:55:28 +0200

    fixup??? reftable: rest of library
    0-sized arrays are actually not portable.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- reftable/stack_test.c
+++ reftable/stack_test.c
@@ -571,11 +571,11 @@ static void test_sizes_to_segments(void)
 
 static void test_sizes_to_segments_empty(void)
 {
-	uint64_t sizes[0];
+	uint64_t sizes[1];
 
 	int seglen = 0;
 	struct segment *segs =
-		sizes_to_segments(&seglen, sizes, ARRAY_SIZE(sizes));
+		sizes_to_segments(&seglen, sizes, 0);
 	assert(seglen == 0);
 	reftable_free(segs);
 }
